a6aaf5695815c0bc6a6dbd4f24608798c6459420
Dana Woodman <dana@danawoodman.com>
2010-12-26 12:25:09 -0800

    Removing special characters from authors name which caused an install issue.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'wapi',
     version = '0.1',
     description = 'Web Api Framework for Django.',
-    author = 'Alberto García Hierro',
+    author = 'Alberto Garcia Hierro',
     author_email = '',
     url = 'https://github.com/fiam/wapi',
     packages = find_packages(),

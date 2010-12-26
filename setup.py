21e794a5d4facc59896cd98a0436784230e16b86
Dana Woodman <dana@danawoodman.com>
2010-12-26 12:22:02 -0800

    Adding setup.py so package can be installed by easy_install/pip/etc...

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wapi',
+    version = '0.1',
+    description = 'Web Api Framework for Django.',
+    author = 'Alberto García Hierro',
+    author_email = '',
+    url = 'https://github.com/fiam/wapi',
+    packages = find_packages(),
+)
